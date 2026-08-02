d91ff92c29df16a009c7e1feca86985be6f1d0e7
Brandon Wright <bwright2810@gmail.com>
2026-08-02 15:58:34 +0000

    Side view: strike zone on right side

--- src/components/Scene3D.tsx
+++ src/components/Scene3D.tsx
@@ -266,8 +266,8 @@ function CameraController({
   const zonePos = new THREE.Vector3(3, 3, 10);
   const zoneLookAt = new THREE.Vector3(0, 2.4, 0.5);
   
-  const sidePos = new THREE.Vector3(-50, 8, 25);
-  const sideLookAt = new THREE.Vector3(0, 4, 10);
+  const sidePos = new THREE.Vector3(-45, 10, 30);
+  const sideLookAt = new THREE.Vector3(5, 3, 15);
 
   useFrame(() => {
     // Side view: OrbitControls takes over
@@ -321,7 +321,7 @@ function SideViewControls({ enabled }: { enabled: boolean }) {
       enableRotate={true}
       minDistance={15}
       maxDistance={200}
-      target={[0, 4, 10]}
+      target={[5, 3, 15]}
     />
   );
 }
